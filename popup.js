7f84ee334973a91e5a4ef1b17e332234503c6c5b
Artem Solovev <artem.solovev.lse@gmail.com>
2017-02-07 15:22:36 +0300

    Added working version of the project

--- popup.js
+++ popup.js
@@ -9,31 +9,30 @@ document.addEventListener( 'DOMContentLoaded', function() {
         var partOfUrl;
 
         var expression = /(https)+[:]+[\//(\w+)]+(github.com)+[\/]+([A-Za-z0-9-_])+[\/]+([A-Za-z0-9-_])+/igm;
-        var exprForPartOfUrl = /(github.com)+[\/]+([A-Za-z0-9-_])+[\/]+([A-Za-z0-9-_])+/igm;
+        var exprForPartOfUrl = /[\/]+([A-Za-z0-9-_])+[\/]+([A-Za-z0-9-_])+/igm;
 
 
         chrome.tabs.query( { 'active': true, 'lastFocusedWindow': true }, function ( tabs ) {
             url = tabs[0].url;
 
             result = url.match( expression ) ? true : false;
+            partOfUrl = tabs[0].url.match( exprForPartOfUrl );
 
-            partOfUrl = tabs[0].url.match( expression );
+            getLinesOfCode( partOfUrl );
+
         });
 
-        document.getElementById( 'counter' ).innerHTML = partOfUrl;
 
-        //getLinesOfCode();
+        function getLinesOfCode( link ) {
+            var apiLink = 'https://api.github.com/repos' + link + '/stats/contributors';
 
-        function getLinesOfCode() {
-            document.getElementById( 'counter' ).innerHTML = 'https://api.' + partOfUrl + '/stats/contributors';
+            document.getElementById( 'counter' ).innerHTML = 'Wait a minute...';
 
-            // Line counter
-            fetch( 'https://api.' + partOfUrl + '/stats/contributors' )
+            fetch( apiLink )
                 .then( response => response.json() )
                 .then( contributors => contributors.map( contributor => contributor.weeks.reduce( ( lineCount, week ) => lineCount + week.a - week.d, 0) ) )
                 .then( lineCounts => lineCounts.reduce( ( lineTotal, lineCount ) => lineTotal + lineCount) )
-            //.then( lines => ( document.getElementById( 'counter' ).innerHTML = result ? lines : 'Something went wrong' ) );
-                .then( lines => ( document.getElementById( 'counter' ).innerHTML = 'boom' ) );
+                .then( lines => ( document.getElementById( 'counter' ).innerHTML = lines ) );
         }
     }, false);
 }, false);
