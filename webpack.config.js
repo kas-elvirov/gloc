627fcfb6dd2a94370e167e555b598bcfb0bf5a43
Artem Solovev <artem.solovev.lse@gmail.com>
2018-04-04 17:11:13 +0300

    Added webpack

---
+++ webpack.config.js
@@ -0,0 +1,144 @@
+const webpack = require('webpack');
+const path = require( 'path' );
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ZipFilesPlugin = require('webpack-zip-files-plugin');
+
+const JSconfig = {
+    name: 'JS',
+    entry: {
+        'background': './src/background.js',
+        'inject': './src/inject.js',
+        'options': './src/options.js',
+        'optipic': './src/optipic.js',
+        'popup': './src/popup.js',
+    },
+    output: {
+        path: path.resolve( __dirname, 'dist/src/' ),
+        filename: '[name].js',
+    },
+    resolveLoader: {
+        modules: [
+            'node_modules',
+        ],
+    },
+    module: {
+        rules: [
+            {
+                use: 'babel-loader',
+                test: /\.js$/,
+            },
+        ],
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            include: /\.js$/,
+            minimize: true,
+        }),
+    ],
+};
+
+const HTMLconfig = {
+    name: 'HTML',
+    entry: {
+        'index': './index.html',
+        'options': './options.html',
+    },
+    output: {
+        path: path.resolve( __dirname, 'dist/' ),
+        filename: '[name].html',
+    },
+    resolveLoader: {
+        modules: [
+            'node_modules',
+        ],
+    },
+    module: {
+        rules: [
+            {
+                use: [{
+                    loader: 'html-loader',
+                }],
+                test: /\.html$/,
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[path][name].[ext]',
+                            emitFile: false,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: __dirname + '/index.html',
+            filename: '/index.html',
+            inject: 'body',
+            hash: true,
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true,
+                html5: true,
+                removeComments: true,
+                removeEmptyAttributes: true,
+                minifyCSS: true,
+            },
+        }),
+        new HtmlWebpackPlugin({
+            template: __dirname + '/options.html',
+            filename: '/options.html',
+            inject: 'body',
+            hash: true,
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true,
+                html5: true,
+                removeComments: true,
+                removeEmptyAttributes: true,
+                minifyCSS: true,
+            },
+        }),
+        new CopyWebpackPlugin([
+            {
+                from: 'img',
+                to: 'img',
+            },
+            {
+                from: 'manifest.json',
+                to: 'manifest.json',
+            },
+        ]),
+    ],
+};
+
+const ZipFiles = {
+    name: 'ZIP',
+    entry: {
+        'inject': './src/inject.js',
+    },
+    output: {
+        path: __dirname + './dist',
+        filename: '[name].zip',
+    },
+    plugins: [
+        new ZipFilesPlugin({
+            entries: [
+                {src: path.join(__dirname, './dist'), dist: '/'},
+            ],
+            output: path.join(__dirname, './build'),
+            format: 'zip',
+        }),
+    ],
+};
+
+module.exports = [
+    JSconfig,
+    HTMLconfig,
+    ZipFiles,
+];
