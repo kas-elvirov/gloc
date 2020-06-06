0a39a5d69020e33fea9720e61b39092fd40b5d1a
Artem Solovev <artem.solovev.lse@gmail.com>
2020-06-06 23:57:11 +0300

    feature/NOTASK Added funding knobs

--- src/modules/Common/helpers/appConfig.ts
+++ src/modules/Common/helpers/appConfig.ts
@@ -2,13 +2,21 @@
 
 interface AppConfig {
 	/**
-	 * base url
+	 * base
 	 */
-	appVersion?: string;
+	appVersion: string;
+
+	/**
+	 * payments
+	 */
+	paypalUrl: string;
+	opencollectiveUrl: string;
 }
 
 const appConfig: AppConfig = {
-	appVersion: process.env.APP_VERSION,
+	appVersion: process.env.APP_VERSION as string,
+	paypalUrl: process.env.PAYPAL_URL as string,
+	opencollectiveUrl: process.env.OPENCOLLECTIVE_URL as string,
 };
 
 export default appConfig;
--- src/modules/Popup/components/Footer/Footer.tsx
+++ src/modules/Popup/components/Footer/Footer.tsx
@@ -3,62 +3,87 @@ import Iframe from 'react-iframe';
 
 import { colors } from '../../../../theme/colors';
 import {
-    Container,
-    Row,
-    Column,
-    Center,
+	Container,
+	Row,
+	Column,
+	Center,
 } from '../../../Common/components/Layout/index';
 import { Text } from '../../../Common/components/Text/index';
 import { Anchor } from '../../../Common/components/Anchor/index';
+import appConfig from '../../../Common/helpers/appConfig';
 
 
 import { decorationProps } from './index';
 
 export default class Footer extends React.PureComponent {
 	render() {
-        const linkMsg = chrome.i18n.getMessage('translateOurApp');
-        // const titleMsg = chrome.i18n.getMessage('attentionPlease');
+		const linkMsg = chrome.i18n.getMessage('translateOurApp');
+		// const titleMsg = chrome.i18n.getMessage('attentionPlease');
 
 		return (
 			<Container>
-                <Column>
-                    {/* <Row>
-                        <Center>
-                            <h3>
-                                {titleMsg}
-                            </h3>
-                        </Center>
-                    </Row> */}
+				<Column>
+					<Row>
+						<Column>
+							<Row>
+								<Center>
+									<Iframe
+										src='https://ghbtns.com/github-btn.html?user=artem-solovev&repo=gloc&type=star&count=true'
+										{...decorationProps.githubFrame}
+									/>
+								</Center>
+							</Row>
 
-                    <Row>
-                        <Center>
-                            <Iframe
-                                src="https://ghbtns.com/github-btn.html?user=artem-solovev&repo=gloc&type=star&count=true"
-                                {...decorationProps.githubFrame}
-                            />
-                        </Center>
-                    </Row>
+							<Row>
+								<Center>
+									<Anchor
+										href='https://goo.gl/forms/uWbvB9tsbOrbaXmz2'
+										target='_blank'
+										color={colors.grey900}
+									>
+										{linkMsg}
+									</Anchor>
+								</Center>
 
-                    <Row>
-                        <Center>
-                            <Anchor
-                                href="https://goo.gl/forms/uWbvB9tsbOrbaXmz2"
-                                target="_blank"
-                                color={colors.grey900}
-                            >
-                                {linkMsg}
-                            </Anchor>
-                        </Center>
+							</Row>
+						</Column>
+						<Column>
+							<Row>
+								<Center>
+									<Anchor
+										href={appConfig.paypalUrl}
+										target='_blank'
+										color={colors.grey900}
+									>
+										Paypal
+									</Anchor>
+								</Center>
+							</Row>
+							<Row>
+								<Center>
+									<Anchor
+										href={appConfig.opencollectiveUrl}
+										target='_blank'
+										color={colors.grey900}
+									>
+										Open collective
+									</Anchor>
+								</Center>
+							</Row>
+						</Column>
+					</Row>
 
-                    </Row>
 
-                    <Row>
-                        <Center>
-                            <Text>Made by <Anchor href="http://artemsolovev.com/" target="_blank" color={colors.pink400}>Artem Solovev</Anchor></Text>
-                        </Center>
-                    </Row>
-                </Column>
-            </Container>
+
+					<Row>
+						<Center>
+							<Text>
+								Made by <Anchor href='http://artemsolovev.com/' target='_blank' color={colors.pink400}>Artem Solovev</Anchor>
+							</Text>
+						</Center>
+					</Row>
+				</Column>
+			</Container>
 		);
 	}
 }
