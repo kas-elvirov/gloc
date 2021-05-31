95ef5469541a919bc2075d832fab089bb7a38055
Artem Solovev <artem.solovev.lse@gmail.com>
2021-05-31 22:04:18 +0300

    feature/#127 popup - initial layout

--- src/modules/Popup/index.tsx
+++ src/modules/Popup/index.tsx
@@ -1,17 +1,26 @@
 import * as React from 'react';
 
-import { Header } from './components/Header/index';
-import { Footer } from './components/Footer/index';
-import { Body } from './components/Body/index';
+import Grid from '@material-ui/core/Grid';
 
 export default class PopupPage extends React.PureComponent {
 	render() {
 		return (
-			<React.Fragment>
-				<Header />
-				<Body />
-				<Footer />
-			</React.Fragment>
+			<Grid
+				container
+				direction="column"
+			>
+				<Grid item>
+					header
+				</Grid>
+
+				<Grid item>
+					header
+				</Grid>
+
+				<Grid item>
+					header
+				</Grid>
+			</Grid>
 		);
 	}
 }
