b3e29369546ea5701dccba859239311aef6f5ba6
Kas Elvirov <kas.elvirov@gmail.com>
2025-01-03 10:44:34 +0300

    removed all legacy code and locales

--- src/_legacy/utils/translateElements.ts
+++
@@ -1,9 +0,0 @@
-export const translateElements = (ids: string[]): void => {
-  ids.forEach((id) => {
-    const element = document.getElementById(id);
-
-    if (element) {
-      element.innerHTML = chrome.i18n.getMessage(id);
-    }
-  });
-};
